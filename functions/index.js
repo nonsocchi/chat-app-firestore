7815a602e599d93a8a0739ebe19ff414dbeeecd0
devnons <unimkeadie@gmail.com>
2022-05-19 10:01:52 +0100

    feat: setup cloud function on Firebase.

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.myFunction = functions.firestore
+    .document("chat/{message}")
+    .onCreate((snap, context) => {
+      // Get an object representing the document
+      // e.g. {'name': 'Marie', 'age': 66}
+      const newValue = snap.data();
+      console.log(newValue);
+
+      // access a particular field as you would any JS property
+      //   const name = newValue.name;
+
+      // perform desired operations ...
+    });
